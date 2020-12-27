87b2f48982e574d9b6e1a3db958dd3765763ac25
Alejandro <alejandro.lr.developer@hotmail.com>
2020-12-27 15:02:21 -0500

    DONE: Redux for user

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,10 +1,12 @@
+import { SET_CURRENT_USER } from './user.types';
+
 const initial_state = {
   currentUser: null,
 };
 
 const userReducer = (state = initial_state, action) => {
   switch (action.type) {
-    case 'SET_CURRENT_USER':
+    case SET_CURRENT_USER:
       return {
         ...state,
         currentUser: action.payload,
